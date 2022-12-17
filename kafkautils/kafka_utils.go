a77db7d6db47146691e2a9d8bbaca7d2369b12cf
Philipp Winter <phw@brave.com>
2022-12-17 07:33:46 -0600

    Embed Amazon's root CA certificate.
    We've been seeing TLS error messages because ia2 wouldn't recognize the
    root CA that signed the certificate chain of the Kafka broker.  This
    commit embeds this root CA certificate in ia2.  It can be found at:
    https://www.amazontrust.com/repository/

--- kafkautils/kafka_utils.go
+++ kafkautils/kafka_utils.go
@@ -28,6 +28,35 @@ const (
 	KafkaRootCert  = "/etc/ssl/cacerts/root_certificate"
 	envKafkaBroker = "KAFKA_BROKERS"
 	envKafkaTopic  = "KAFKA_TOPIC"
+	// amazonRootCACert is the certificate of one of Amazon's root CAs.  The
+	// certificate chain that we encounter when connecting to our Kafka broker
+	// goes up to this CA.  The root certificates are available at:
+	// https://www.amazontrust.com/repository/
+	amazonRootCACert = `
+-----BEGIN CERTIFICATE-----
+MIID7zCCAtegAwIBAgIBADANBgkqhkiG9w0BAQsFADCBmDELMAkGA1UEBhMCVVMx
+EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxJTAjBgNVBAoT
+HFN0YXJmaWVsZCBUZWNobm9sb2dpZXMsIEluYy4xOzA5BgNVBAMTMlN0YXJmaWVs
+ZCBTZXJ2aWNlcyBSb290IENlcnRpZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTA5
+MDkwMTAwMDAwMFoXDTM3MTIzMTIzNTk1OVowgZgxCzAJBgNVBAYTAlVTMRAwDgYD
+VQQIEwdBcml6b25hMRMwEQYDVQQHEwpTY290dHNkYWxlMSUwIwYDVQQKExxTdGFy
+ZmllbGQgVGVjaG5vbG9naWVzLCBJbmMuMTswOQYDVQQDEzJTdGFyZmllbGQgU2Vy
+dmljZXMgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkgLSBHMjCCASIwDQYJKoZI
+hvcNAQEBBQADggEPADCCAQoCggEBANUMOsQq+U7i9b4Zl1+OiFOxHz/Lz58gE20p
+OsgPfTz3a3Y4Y9k2YKibXlwAgLIvWX/2h/klQ4bnaRtSmpDhcePYLQ1Ob/bISdm2
+8xpWriu2dBTrz/sm4xq6HZYuajtYlIlHVv8loJNwU4PahHQUw2eeBGg6345AWh1K
+Ts9DkTvnVtYAcMtS7nt9rjrnvDH5RfbCYM8TWQIrgMw0R9+53pBlbQLPLJGmpufe
+hRhJfGZOozptqbXuNC66DQO4M99H67FrjSXZm86B0UVGMpZwh94CDklDhbZsc7tk
+6mFBrMnUVN+HL8cisibMn1lUaJ/8viovxFUcdUBgF4UCVTmLfwUCAwEAAaNCMEAw
+DwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYwHQYDVR0OBBYEFJxfAN+q
+AdcwKziIorhtSpzyEZGDMA0GCSqGSIb3DQEBCwUAA4IBAQBLNqaEd2ndOxmfZyMI
+bw5hyf2E3F/YNoHN2BtBLZ9g3ccaaNnRbobhiCPPE95Dz+I0swSdHynVv/heyNXB
+ve6SbzJ08pGCL72CQnqtKrcgfU28elUSwhXqvfdqlS5sdJ/PHLTyxQGjhdByPq1z
+qwubdQxtRbeOlKyWN7Wg0I8VRw7j6IPdj/3vQQF3zCepYoUz8jcI73HPdwbeyBkd
+iEDPfUYd/x7H4c7/I9vG+o1VTqkC50cRRj70/b17KSa7qWFiNyi2LSr2EIZkyXCn
+0q23KXB56jzaYyWf/Wi3MOxw+3WKt21gZ7IeyLnp2KhvAotnDU0mV3HaIPzBSlCN
+sSi6
+-----END CERTIFICATE-----`
 )
 
 var l = log.New(os.Stderr, "kafkautils: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
@@ -61,34 +90,25 @@ func NewKafkaWriter(certFile, keyFile string) (*kafka.Writer, error) {
 	}
 	l.Printf("Fetched Kafka topic %q from environment variable.", kafkaTopic)
 
-	f, _ := os.ReadFile(certFile)
-	l.Printf("Client certificate file:\n%s", f)
-	f, _ = os.ReadFile(keyFile)
-	l.Printf("Client key file:\n%s", f)
-
+	// Client certificate.
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		return nil, err
 	}
-	l.Println("Loaded certificate and key file for Kafka.")
-
+	// Server certificate chain.
 	rawInterCert, err := os.ReadFile(KafkaInterCert)
 	if err != nil {
 		return nil, err
 	}
-	l.Printf("CA intermediate cert:\n%s\n", rawInterCert)
-
 	rawInterChain, err := os.ReadFile(KafkaInterChain)
 	if err != nil {
 		return nil, err
 	}
-	l.Printf("CA intermediate cert chain:\n%s\n", rawInterChain)
-
 	rawRootCert, err := os.ReadFile(KafkaRootCert)
 	if err != nil {
 		return nil, err
 	}
-	l.Printf("CA certificate:\n%s\n", rawRootCert)
+	l.Println("Loaded client and server certificates from files.")
 
 	ourRootCAs, err := x509.SystemCertPool()
 	if err != nil {
@@ -103,6 +123,9 @@ func NewKafkaWriter(certFile, keyFile string) (*kafka.Writer, error) {
 	}
 	if ok := ourRootCAs.AppendCertsFromPEM(rawRootCert); !ok {
 		return nil, errors.New("failed to parse root certificate")
+	}
+	if ok := ourRootCAs.AppendCertsFromPEM([]byte(amazonRootCACert)); !ok {
+		return nil, errors.New("failed to parse Amazon root certificate")
 	}
 
 	l.Printf("Creating Kafka writer for %q using topic %q.", kafkaBroker, kafkaTopic)
