2c41c9ea127b753b40455195592d4c11f9494191
Philipp Winter <phw@brave.com>
2023-06-08 10:44:31 -0500

    Set fd soft limit to hard limit.
    This will help tokenizer cope with the large number of ads connections.

--- util.go
+++ util.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"syscall"
+
 	"github.com/linkedin/goavro/v2"
 )
 
@@ -11,4 +13,24 @@ func avroEncode(codec *goavro.Codec, blob []byte) ([]byte, error) {
 	}
 	binary, err := codec.BinaryFromNative(nil, native)
 	return binary, err
+}
+
+// maxSoftFdLimit raises the file descriptor soft limit to the hard limit.
+func maxSoftFdLimit() error {
+	var rLimit = new(syscall.Rlimit)
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, rLimit); err != nil {
+		return err
+	}
+	l.Printf("Original fd limit: cur=%d, max=%d", rLimit.Cur, rLimit.Max)
+
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, rLimit); err != nil {
+		return err
+	}
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, rLimit); err != nil {
+		return err
+	}
+	l.Printf("Modified fd limit: cur=%d, max=%d.", rLimit.Cur, rLimit.Max)
+
+	return nil
 }
